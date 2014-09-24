6e359b751a9dde4afa5a9362adfde9cf6f3b22e1
gregerg <gregerg@gmail.com>
2014-09-24 15:58:38 -0700

    added SongQueueView/EntryView to index, and made render functions

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,11 +1,28 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: "table",
+
   initialize: function() {
+    this.render();
   },
 
   render: function() {
-    return this.$el;
+    this.$el.children().detach();
+
+    this.$el.html('<th>Song Queue</th>').append(
+      this.collection.map(function(song){
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
   }
 
 });
+
+
+// this.$el.children().detach();
+
+// this.$el.html('<th>Library</th>').append(
+//   this.collection.map(function(song){
+//     return new LibraryEntryView({model: song}).render();
+//   })
